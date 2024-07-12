b31f1a9274215e9c698b3d401e176807d0725961
oussama charafi <ou.charafi@gmail.com>
2024-07-12 22:49:55 +0100

    I added the verification of all requestes token

--- src/main/java/com/bank/config/ConfigSecurity.java
+++ src/main/java/com/bank/config/ConfigSecurity.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +37,7 @@ public class ConfigSecurity {
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->formLogin.disable()); // Désactiver le formulaire de login par défaut de Spring Security
+        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
